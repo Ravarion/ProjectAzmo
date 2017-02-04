5ad056eb37c792ca903c980d9631480c17ff2169
Ravarion <steven.d.lindbloom@gmail.com>
2017-02-03 22:35:15 -0600

    Added GrabParent and HandController Scripts

---
+++ ProjectAzmo/Assets/Scripts/GrabParent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabParent : MonoBehaviour {
+
+    public bool grabbed = false;
+    public bool rightHand = false;
+
+    public Vector3 oldPos;
+
+    void Update()
+    {
+        if (grabbed)
+        {
+            oldPos = transform.position;
+            transform.position = rightHand ? GameObject.Find("Controller (right)").transform.position : GameObject.Find("Controller (right)").transform.position;
+        }
+    }
+}
