bb890ae256656fa1e496480236fce7b3a2ead2fd
Scoopadoop <giesfeldte0427@uwstout.edu>
2017-02-05 13:35:01 -0600

    Added basic win/loss function

---
+++ ProjectAzmo/Assets/Scripts/Win-Loss.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win_Loss : MonoBehaviour {
+
+   private string endText = "";
+
+    // Use this for initialization
+    void Start () {
+        GameObject.DontDestroyOnLoad(gameObject);
+	}
+
+    void Win(int soldiersKilled)
+    {
+        endText = "After you murdered " + soldiersKilled + " soldiers, you ravaged the kingdom and left their civilization in ruins. You monster.";
+        SceneManager.LoadScene(1); //fade into game over screen/scene
+    }
+
+    void Lose(int soldiersKilled, int whyDidYouDie)
+    {
+        if(whyDidYouDie == 0)           //Death by Exploding Human
+        {
+            endText = "After you murdered " + soldiersKilled + " soldiers, you were slain by a soldier";
+        }
+        else if(whyDidYouDie == 1)      //Death by Cannonball
+        {
+            endText = "After you murdered " + soldiersKilled + " soldiers, you were slain from afar";
+        }
+        else if(whyDidYouDie == 2)      //Death by Bonfire
+        {
+            endText = "After you murdered " + soldiersKilled + " soldiers, the beacon's were lit, and you were outnumbered";
+        }
+
+        SceneManager.LoadScene(1); //fade into game over screen/scene
+        //enable option to go back to main menu (throw a human at the option?)
+    }
+}
---
+++ ProjectAzmo/Assets/Scripts/Win_Loss.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win_Loss : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject.DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Win()
+    {
+
+    }
+
+    void Lose(int whyDidYouDie)
+    {
+        string endText = "";
+        if(whyDidYouDie == 0)           //Death by Exploding Human
+        {
+            endText = "You have been slain by a soldier";
+        }
+        else if(whyDidYouDie == 1)      //Death by Cannonball
+        {
+            endText = "You have been slain from afar";
+        }
+        else if(whyDidYouDie == 2)      //Death by Bonfire
+        {
+            endText = "The beacon's were lit, and you were outnumbered";
+        }
+
+        SceneManager.LoadScene(""); //fade into game over screen/scene
+        //enable option to go back to main menu (throw a human at the option?)
+    }
+}
